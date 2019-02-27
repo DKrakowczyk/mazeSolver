88eb1968e213dd5da14ffd929af386a97572471e
Dawid Krakowczyk <35225469+DKrakowczyk@users.noreply.github.com>
2019-02-27 13:35:41 +0100

    Nodes placement added

--- src/mazesolver/Frame.java
+++ src/mazesolver/Frame.java
@@ -1,14 +1,30 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package mazesolver;
+
+import java.awt.*;
+import javax.swing.*;
 
 /**
  *
  * @author Dawid
  */
-public class Frame {
+public class Frame extends JFrame{
+    public Frame(){
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setTitle("Maze Solver");
+        setResizable(false);
+        init();
+    }
+    
+    public void init(){
+        setLayout(new GridLayout(1,1,0,0));
+        Maze m = new Maze();
+        add(m);
+        pack();
+        setVisible(true);
+    }
+    
+    public static void main(String[] args){
+        new Frame();
+    }
     
 }
