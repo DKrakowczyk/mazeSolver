2b89ea32be91770ccf4647346852e8a630d1b23a
Michał Kucharski <M.Kucharskov@onet.pl>
2019-02-27 18:51:02 +0100

    GUI improvement

--- src/mazesolver/gui/GUI.java
+++ src/mazesolver/gui/GUI.java
@@ -30,7 +30,7 @@ public class GUI extends JPanel implements MouseListener, MouseMotionListener {
         this.lastX = -1;
         this.lastY = -1;
         this.grid = new Grid();
-        
+
         setPreferredSize(new Dimension(MazeSolver.width, MazeSolver.height));
         setFocusable(true);
         addMouseListener(this);
@@ -41,51 +41,55 @@ public class GUI extends JPanel implements MouseListener, MouseMotionListener {
     @Override
     public void paint(Graphics g) {
         grid.draw(g);
-        
-        if(!showMenu){
+
+        if (!showMenu) {
             drawMenuHidden(g);
-        } else if(showMenu) {
+        } else if (showMenu) {
             drawMenu(g);
         }
     }
-    
-    public void drawMenuHidden(Graphics g){
+
+    public void drawMenuHidden(Graphics g) {
         g.setColor(new Color(41, 43, 45, 160));
         g.fillRect(20, 540, 40, 40);
         g.setColor(Color.WHITE);
-        g.setFont(new Font("Arial", Font.PLAIN, 20));
-        g.drawString("(M)", 25, 567);
+        g.setFont(new Font("Arial", Font.PLAIN, 16));
+        g.drawString("(M)", 29, 566);
     }
-    
-    public void drawMenu(Graphics g){
+
+    public void drawMenu(Graphics g) {
         g.setColor(new Color(41, 43, 45, 160));
         g.fillRect(20, 480, 460, 100);
         g.setColor(Color.white);
-        g.setFont(new Font("Impact", Font.PLAIN, 16));
-        if(setStart)
-        g.setColor(new Color(7, 101, 238));
+        g.setFont(new Font("Arial", Font.PLAIN, 16));
+        if (setStart) {
+            g.setColor(new Color(7, 101, 238));
+        }
         g.drawString("Mouse + S key: set start", 25, 500);
         g.setColor(Color.white);
-        if(setEnd)
-        g.setColor(new Color(238, 29, 7));
+        if (setEnd) {
+            g.setColor(new Color(238, 29, 7));
+        }
         g.drawString("Mouse + E key: set end", 25, 520);
         g.setColor(Color.white);
-        if(mouseInUse)
-        g.setColor(new Color(61, 201, 7));
+        if (mouseInUse) {
+            g.setColor(new Color(61, 201, 7));
+        }
         g.drawString("Mouse left/right: add/remove walls", 25, 540);
         g.setColor(Color.white);
-        if(algorithmStarted)
-        g.setColor(new Color(173, 100, 52));
+        if (algorithmStarted) {
+            g.setColor(new Color(173, 100, 52));
+        }
         g.drawString("Space: run/stop choosen algorithm", 140, 570);
         g.setColor(Color.white);
         g.drawLine(260, 480, 260, 540);
-        
+
         // colors todo
         g.drawString("1: A STAR", 270, 500);
         g.drawString("2: BFS (Breadth first search)", 270, 520);
         g.drawString("3: DFS (Depth first search)", 270, 540);
     }
-    
+
     @Override
     public void mouseClicked(MouseEvent me) {
         int x = (me.getX()) / MazeSolver.nodeSize;
@@ -102,14 +106,14 @@ public class GUI extends JPanel implements MouseListener, MouseMotionListener {
         } else if (SwingUtilities.isRightMouseButton(me)) {
             grid.setEmpty(x, y);
         }
-        
+
         this.repaint();
     }
 
     @Override
     public void mousePressed(MouseEvent me) {
-         mouseInUse = true;
-         this.repaint();
+        mouseInUse = true;
+        this.repaint();
     }
 
     @Override
@@ -125,7 +129,7 @@ public class GUI extends JPanel implements MouseListener, MouseMotionListener {
     @Override
     public void mouseExited(MouseEvent me) {
         mouseInUse = false;
-        repaint();
+        this.repaint();
     }
 
     @Override
@@ -160,7 +164,7 @@ public class GUI extends JPanel implements MouseListener, MouseMotionListener {
             lastY = y;
         }
         mouseInUse = true;
-        
+
         this.repaint();
     }
 
