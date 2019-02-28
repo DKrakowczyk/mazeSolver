19b5a86607cc8bca9b580913a040ea5378930dd3
Michał Kucharski <M.Kucharskov@onet.pl>
2019-02-28 10:19:21 +0100

    Separate generator interface

--- src/mazesolver/generator/NoiseGenerator.java
+++ src/mazesolver/generator/NoiseGenerator.java
@@ -12,20 +12,30 @@ import mazesolver.grid.Node;
  *
  * @author DKrakowczyk & M. Kucharskov
  */
-public class Generator {
+public class NoiseGenerator implements IGenerator {
 
-    public static void generate(Grid grid) {
+    double noise = 0.3;
+
+    public void setNoise(double noise) {
+        if (noise < 0 || noise > 1) {
+            return;
+        }
+
+        this.noise = noise;
+    }
+
+    @Override
+    public void generate(Grid grid) {
         int rows = grid.getRows();
         int cols = grid.getCols();
-        double fillness = 0.3;
 
         // Erase grid
         grid.erase();
-        
+
         //Simple grid noise generator
-        for (int y = 0;  y < rows; y++) {
+        for (int y = 0; y < rows; y++) {
             for (int x = 0; x < cols; x++) {
-                if(Math.random() < fillness) {
+                if (Math.random() < noise) {
                     Node n = grid.getNode(x, y);
                     n.setType(Node.Types.WALL);
                 }
