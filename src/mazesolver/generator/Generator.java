68c11527160d841fb454e547e71d505e0a4186ae
Michał Kucharski <M.Kucharskov@onet.pl>
2019-02-28 10:14:52 +0100

    Create simple grid noise generator

---
+++ src/mazesolver/generator/Generator.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mazesolver.generator;
+
+import mazesolver.grid.Grid;
+import mazesolver.grid.Node;
+
+/**
+ *
+ * @author M.Kucharskov
+ */
+public class Generator {
+
+    public static void generate(Grid grid) {
+        int rows = grid.getRows();
+        int cols = grid.getCols();
+        double fillness = 0.3;
+
+        // Erase grid
+        grid.erase();
+        
+        //Simple grid noise generator
+        for (int y = 0;  y < rows; y++) {
+            for (int x = 0; x < cols; x++) {
+                if(Math.random() < fillness) {
+                    Node n = grid.getNode(x, y);
+                    n.setType(Node.Types.WALL);
+                }
+            }
+        }
+
+    }
+}
